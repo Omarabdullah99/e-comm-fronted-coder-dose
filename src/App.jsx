81361ebe89e755d12d36fd2522daf642e75c8d09
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-09-27 10:12:54 +0600

    react router dom setup and Login and Sign up page design

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { useSelector, useDispatch } from 'react-redux'
 import Home from './page/Home'
+import LoginPage from './page/LoginPage'
+import SignupPage from './page/SignupPage'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 
 function App() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
 
   return (
     <>
-    <Home></Home>
+   
+   <RouterProvider router={router}></RouterProvider>
  
     </>
   )
---
+++ src/page/SignupPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from '../features/auth/component/SignUp'
+
+const SignupPage = () => {
+  return (
+    <div>
+        <Signup></Signup>
+    </div>
+  )
+}
+
+export default SignupPage
